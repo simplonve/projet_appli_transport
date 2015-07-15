4a13a5576818bc3731f08269606ce48d0edbcaef
thalion26 <thalion26@gmail.com>
2015-07-15 17:20:22 +0200

    ajout champs text input

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ from kivy.core.window import Window
 from kivy.uix.widget import Widget
 from kivy.uix.button import Button
 from kivy.uix.label import Label
+from kivy.uix.textinput import TextInput
 from kivy.graphics import Color, Rectangle
 from kivy.uix.floatlayout import FloatLayout
 from kivy.properties import NumericProperty, ReferenceListProperty,\
@@ -34,6 +35,22 @@ class ClassiqueApp(App):
 						font_size= 50,
 						Color= (1, 0, 0),
 						pos_hint={'x': 0, 'center_y': 0.9})
+		depart = Label(	text='[color=2ecc71]Départ[/color]',
+						markup= True,
+						font_size= 20,
+						Color= (1, 0, 0),
+						pos_hint={'x': -0.25, 'center_y': 0.55})
+		arrive = Label(	text='[color=2ecc71]Arrivée[/color]',
+						markup= True,
+						font_size= 20,
+						Color= (1, 0, 0),
+						pos_hint={'x': 0.25, 'center_y': 0.55})
+		self.champs1 = TextInput(	multiline=True,
+									size_hint=(.3, .04),
+									pos_hint={'x': .1, 'center_y':.5})
+		self.champs2 = TextInput(	multiline=True,
+									size_hint=(.3, .04),
+									pos_hint={'x': .6, 'center_y':.5})
 		btn1 = Button(	text='[color=2ecc71]Valider Recherche[/color]',
 						font_size_hint= 0.2,
 						markup= True,
@@ -45,6 +62,10 @@ class ClassiqueApp(App):
 						size_hint=(.2,.1),
 						pos_hint={'x': 0.6, 'center_y': .1})
 		parent.add_widget(titre)
+		parent.add_widget(depart)
+		parent.add_widget(arrive)
+		parent.add_widget(self.champs1)
+		parent.add_widget(self.champs2)
 		parent.add_widget(btn1)
 		parent.add_widget(btn2)
 		return parent
