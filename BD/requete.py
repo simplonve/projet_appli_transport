775c4419434979e0df948b0450fe944f70cb3d8b
TheSystem-69 <ethancmk@hotmail.fr>
2015-07-21 13:38:11 +0200

    premier commit requete

--- BD/requete.py
+++ BD/requete.py
@@ -1,19 +1,63 @@
 #! /usr/bin/python
 # -*- coding:utf-8 -*-
 
-import lignes
+from lignes import *
+from time import localtime, strftime
 
+def recup_list_ville():
+    '''recupère la liste des villes et la renvoi sous forme de tableau'''
+    ville_aller = []
+    ville_retour = []
 
+    for i in range(len(Le_Cheylard_Valence[0])):
+        if Le_Cheylard_Valence[0][i][0] not in ville_aller:
+            ville_aller.append(Le_Cheylard_Valence[0][i][0])
 
+    for i in range(len(Valence_le_cheylard[0])):
+        if Valence_le_cheylard[0][i][0] not in ville_retour:
+            ville_retour.append(Valence_le_cheylard[0][i][0])
 
+    ville_aller.pop(0)
+    ville_retour.pop(0)
+    return ville_aller, ville_retour
 
+'''valeur d'entrée'''
+periode = 'scol'
+aujourdhui = 'M'
+depart = 'LE CHEYLARD'
+arriver = 'CHARMES'
+heure = strftime("%H:%M", localtime())
 
+index_jour = []
 
+ville_aller, ville_retour = recup_list_ville()
 
+'''recup le sens voulu'''
+if depart in ville_aller and arriver in ville_aller:
+    if ville_aller.index(depart) < ville_aller.index(arriver):
+        fiche_horaire = Le_Cheylard_Valence
+    else:
+        fiche_horaire = Valence_le_cheylard
 
+'''recup la fiche horaire pour la bonne periode'''
+for i in range(2):
+    if fiche_horaire[i][0][0] == periode:
+        fiche_horaire = fiche_horaire[i]
 
+'''recup index jour pour selectionner les bons horaires'''
+for jour in fiche_horaire[0]:
+    if aujourdhui in jour:
+        index_jour.append(fiche_horaire[0].index(jour))
 
+'''recup lignes contenant depart et arriver'''
+for row in fiche_horaire:
+    if row[0] == depart:
+        for index in index_jour:
+            print(row)
+    elif row[0] == arriver:
+        print(row)
 
+print(index_jour)
 
 
 
--- BD/traitement.py
+++ BD/traitement.py
@@ -26,48 +26,57 @@ def lecture_fichier(path):
 
 def suppr_inutile(tableau):
     '''supprimer les colonnes inutile (contenant ND(non désservi))'''
-    index_to_pop = []
+    index_a_supprimer = []
     first = True
-    for i in range(len(tableau[0])-2):
-        if tableau[0][i] == 'ND':
-            tableau[0].pop(i)
-            index_to_pop.append(i)
-
+    for element in tableau[0]:
+        if element == 'ND':
+            index_a_supprimer.append(tableau[0].index('ND'))
+            tableau[0].pop(tableau[0].index('ND'))
     for row in tableau:
-        if first == True or len(row) == len(tableau[0]):
+        print(row)
+        if first == True:
             first = False
             continue
-        for to_pop in index_to_pop:
-            row.pop(to_pop)
-
+        for index in index_a_supprimer:
+            row.pop(index)
+        print('----------------------------------------')
+        print(row)
+        print('++++++++++++++++++++++++++++++++++++++++')
+    print('fin du traitement de '+tableau[0][0])
     return tableau
 
-'''chemin du fichier a traiter'''
-path = 'st sauveur le cheylard.txt'
+def main():
+    '''chemin du fichier a traiter'''
+    path = 'Le Cheylard Valence.csv'
 
-fichier = lecture_fichier(path)
+    fichier = lecture_fichier(path)
 
-'''Création de trois tableaux
-pour chaque "catégories"
-scol = periode scolaire
-...'''
-scol = [fichier[0]] + fichier[3:]
-vac_ete = [fichier[1]] + fichier[3:]
-autre_vac = [fichier[2]] + fichier[3:]
+    '''Création de trois tableaux
+    pour chaque "catégories"
+    scol = periode scolaire
+    ...'''
+    scol = [fichier[0]] + fichier[3:]
+    vac_ete = [fichier[1]] + fichier[3:]
+    autre_vac = [fichier[2]] + fichier[3:]
 
-scol = suppr_inutile(scol)
-autre_vac = suppr_inutile(autre_vac)
-vac_ete = suppr_inutile(vac_ete)
+    scol = suppr_inutile(scol)
+    autre_vac = suppr_inutile(autre_vac)
+    for row in vac_ete:
+        print(row)
+    #vac_ete = suppr_inutile(vac_ete)
 
-'''decoupe du chemin du fichier pour enlever espace et extension'''
-path = path.split(' ')
-path = path[:len(path)-1]+path[len(path)-1].split('.')
-path.pop(len(path)-1)
-path = '_'.join(path)
+    '''decoupe du chemin du fichier pour enlever espace et extension'''
+    path = path.split(' ')
+    path = path[:len(path)-1]+path[len(path)-1].split('.')
+    path.pop(len(path)-1)
+    path = '_'.join(path)
 
-'''inscrit les tableaux dans lignes.py
-path fichier + = [ + les trois tableau + ]
-avec un saut de ligne après chaque tableaux
-'''
-with open('lignes.py', 'a') as bd:
-    bd.write(path+' = [\n'+str(scol)+',\n'+str(autre_vac)+',\n'+str(vac_ete)+']')
+    '''inscrit les tableaux dans lignes.py
+    path fichier + = [ + les trois tableau + ]
+    avec un saut de ligne après chaque tableaux
+    '''
+    with open('lignes.py', 'a') as bd:
+        bd.write(path+' = [\n'+str(scol)+',\n'+str(autre_vac)+',\n'+str(vac_ete)+']\n')
+
+if __name__ == '__main__':
+    main()
