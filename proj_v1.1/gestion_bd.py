7a2b474bb9d0b2570343f019df37c37dae30156e
TheSystem-69 <ethancmk@hotmail.fr>
2015-07-22 01:19:17 +0200

    optimisation heure, selection ville + incorporation insert_bd dans gestion_bd

--- proj_v1.1/gestion_bd.py
+++ proj_v1.1/gestion_bd.py
@@ -36,6 +36,90 @@ heure = int(str(heure[0])+str(heure[1]))
 from Base_de_donnees import *
 from time import localtime, strftime
 
+###############################################################
+'''section insertion'''
+
+def lecture_fichier(path):
+    '''lit un csv ou un txt et le renvoi sous forme de tableau.'''
+    with open(path, 'r') as fichier:
+        read_fichier = fichier.read()
+
+    tab_fichier = []
+    saut_de_ligne = True
+
+    while saut_de_ligne:
+        if '\n' in read_fichier:
+            index = read_fichier.index('\n')
+            tab_fichier.append([read_fichier[:index]])
+            read_fichier = read_fichier[index+1:]
+        else:
+            saut_de_ligne = False
+
+    for j in range(len(tab_fichier)):
+        ligne_tab = ';'.join(tab_fichier[j])
+        ligne_tab = ligne_tab.split(';')
+        tab_fichier[j] = ligne_tab
+
+    return tab_fichier
+
+def suppr_inutile(tableau):
+    '''supprimer les colonnes inutile (contenant ND(non désservi))'''
+    index = None
+    IsND = True
+    while IsND:
+        if 'ND' not in tableau[0]:
+            IsND = False
+            break
+        for row in tableau:
+            if len(row) == len(tableau[0]) and len(row) <= 8:
+                break
+            if row == tableau[0] and 'ND' in row:
+                index = tableau[0].index('ND')
+                tableau[0].pop(index)
+            else:
+               row.pop(index)
+
+    return tableau
+
+def insert():
+    '''chemin du fichier a traiter'''
+    path = input('nom du fichier a inserer')
+    path = 'sources/'+path
+
+    '''Création de trois tableaux
+    pour chaque "catégories"
+    scol = periode scolaire
+    ...'''
+    fichier = lecture_fichier(path)
+
+    scol = [fichier[0]] + fichier[3:]
+    scol = suppr_inutile(scol)
+
+    fichier = lecture_fichier(path)
+
+    autre_vac = [fichier[2]] + fichier[3:]
+    autre_vac = suppr_inutile(autre_vac)
+
+    fichier = lecture_fichier(path)
+
+    vac_ete = [fichier[1]] + fichier[3:]
+    vac_ete = suppr_inutile(vac_ete)
+
+
+    '''decoupe du chemin du fichier pour enlever espace et extension'''
+    path = path.split(' ')
+    path = path[:len(path)-1]+path[len(path)-1].split('.')
+    path.pop(len(path)-1)
+    path = '_'.join(path)
+
+    '''inscrit les tableaux dans Base_de_donnees.py
+    path fichier + = [ + les trois tableau + ]
+    avec un saut de ligne après chaque tableaux
+    '''
+    with open('Base_de_donnees.py', 'a') as bd:
+        bd.write(path+' = [\n'+str(scol)+',\n'+str(autre_vac)+',\n'+str(vac_ete)+']\n')
+
+###############################################################
 def select_ville():
     '''recupère la liste des villes et la renvoi sous forme de tableau'''
     ville_aller = []
@@ -43,11 +127,11 @@ def select_ville():
 
     for i in range(len(Le_Cheylard_Valence[0])):
         if Le_Cheylard_Valence[0][i][0] not in ville_aller:
-            ville_aller.append('Ville : '+Le_Cheylard_Valence[0][i][0]+' Arret : '+Le_Cheylard_Valence[0][i][1])
+            ville_aller.append(Le_Cheylard_Valence[0][i][0]+' - '+Le_Cheylard_Valence[0][i][1])
 
     for i in range(len(Valence_le_cheylard[0])):
         if Valence_le_cheylard[0][i][0] not in ville_retour:
-            ville_retour.append('Ville : '+Valence_le_cheylard[0][i][0]+' Arret : '+Valence_le_cheylard[0][i][1])
+            ville_retour.append(Valence_le_cheylard[0][i][0]+' - '+Valence_le_cheylard[0][i][1])
 
     ville_aller.pop(0)
     ville_retour.pop(0)
