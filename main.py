1d069dfe0551eb7c7a9e1e4e93b0b7e350ab5290
TheSystem-69 <ethancmk@hotmail.fr>
2015-07-27 19:57:47 +0200

    recup ville

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ kivy.require('1.9.0')
 
 import gestion_bd
 from kivy.app import App
+from kivy.properties import Property
 from kivy.core.window import Window
 from kivy.uix.widget import Widget
 from kivy.uix.button import Button
@@ -39,12 +40,13 @@ class MainApp(App):
     def ville_depart(self, value):
         self.parent.remove_widget(self.titre)
         self.parent.remove_widget(self.bouton)
-        liste_ville = ListeVille()
+        liste_ville = ListeVille(self.parent)
         self.parent.add_widget(liste_ville.list_view)
 
 class ListeVille(MainApp):
-    def __init__(self, **kwargs):
+    def __init__(self, parent,**kwargs):
         super(MainApp, self).__init__(**kwargs)
+        self.parent = parent
         self.orientation = 'vertical'
         self.ville_depart = None
         self.ville_arriver = None
@@ -58,10 +60,8 @@ class ListeVille(MainApp):
         self.list_adapter.bind(on_selection_change=self.selection_change)
 
         self.list_view = ListView(adapter=self.list_adapter)
-        #self.add_widget(list_view)
 
     def selection_change(self, adapter, *args):
-        print ('---- selection change')
         for element in adapter.selection:
             x = element.index #index du truc selectionner
             print(adapter.data[x])
@@ -71,12 +71,30 @@ class ListeVille(MainApp):
                 self.ville_depart = adapter.data[x]
                 self.list_adapter.data = gestion_bd.select_seconde_ville(str(adapter.data[x]))
             if self.ville_arriver != None:
-                print(gestion_bd.select_horaire(self.ville_depart, self.ville_arriver))
-                #self.list_adapter.data = gestion_bd.select_horaire(str(self.ville_depart, self.ville_arriver))
+                self.retour = gestion_bd.select_horaire(self.ville_depart, self.ville_arriver)
+                self.parent.remove_widget(self.list_view)
+                affichage = Affichage(self.parent, self.ville_depart, self.ville_arriver, self.retour)
+                self.parent.add_widget(affichage.affichage_ville_depart)
+                self.parent.add_widget(affichage.affichage_ville_arriver)
 
 class Affichage(MainApp):
-    def __init__(self, **kwargs):
+    def __init__(self, parent, ville_depart, ville_arriver, retour, **kwargs):
         super(MainApp, self).__init__(**kwargs)
+        self.parent = parent
+        self.retour = retour
+        self.affichage_ville_depart = Label(text=ville_depart,
+                    markup= True,
+                    font_size= 50,
+                    pos_hint={'x': 0, 'center_y': 0.98})
+        self.affichage_ville_arriver = Label(text=ville_arriver,
+                    markup= True,
+                    font_size= 50,
+                    pos_hint={'x': 0, 'center_y': 0.5})
+
+        for ville in self.retour:
+            for bus in self.retour[ville]:
+                print(self.retour[ville][bus])
+
 
 if __name__ == '__main__':
     MainApp().run()
